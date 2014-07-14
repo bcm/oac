e20312941696223b05f6bcaf26eb03562fa4be81
Brian Moseley <bcm@maz.org>
2014-07-14 16:25:43 -0700

    add various calendar subscribe options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+require 'uri'
+
 module ApplicationHelper
+  URI_ESCAPE_URL_REGEX = Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")
+  def uri_escape_url(url)
+    URI.escape(url, URI_ESCAPE_URL_REGEX)
+  end
 end
