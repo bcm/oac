5dbcf47ab2d3dab5096a9b31304034e88c089cc5
Brian Moseley <bcm@maz.org>
2014-07-14 14:00:51 -0700

    fix tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,5 +11,30 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before(:each) do
+    if example.metadata[:js]
+      Capybara.current_driver = :selenium
+      DatabaseCleaner.strategy = :truncation
+    elsif example.metadata[:after_commit]
+      # these tests need to not run within a transaction in order to allow after_commit callbacks to fire or to
+      # let transactions with row locks commit
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.start
+    end
+  end
+
+  config.after(:each) do
+    ActionMailer::Base.deliveries.clear
+    Capybara.use_default_driver if example.metadata[:js]
+    DatabaseCleaner.clean
+  end
+
   config.infer_spec_type_from_file_location!
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,29 +22,4 @@ RSpec.configure do |config|
     mocks.syntax = :expect
     mocks.verify_partial_doubles = true
   end
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before(:each) do
-    if example.metadata[:js]
-      Capybara.current_driver = :selenium
-      DatabaseCleaner.strategy = :truncation
-    elsif example.metadata[:after_commit]
-      # these tests need to not run within a transaction in order to allow after_commit callbacks to fire or to
-      # let transactions with row locks commit
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.start
-    end
-  end
-
-  config.after(:each) do
-    ActionMailer::Base.deliveries.clear
-    Capybara.use_default_driver if example.metadata[:js]
-    DatabaseCleaner.clean
-  end
 end
