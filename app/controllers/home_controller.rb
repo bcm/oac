f19de57dca040149f74346d32e69ef3b76adf595
Brian Moseley <bcm@maz.org>
2014-07-14 22:32:55 -0700

    order by category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     @prev_date = @date.beginning_of_month - 1
     @next_date = @date.end_of_month + 1
     period = [@date.beginning_of_month.beginning_of_week, @date.end_of_month.end_of_week]
-    trips = Trip.in_period(*period)
+    trips = Trip.in_period(*period).order(category: :asc)
     defaults = (period.first..period.last).each_with_object({}) { |date, m| m[date] = [] }
     @trips = trips.each_with_object(defaults) do |trip, m|
       (trip.starts_on..trip.ends_on).each { |date| m[date] ||= []; m[date] << trip }
