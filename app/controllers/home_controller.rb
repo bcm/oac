23d9bef478b00df53d826ca77906aa447bf50940
Brian Moseley <bcm@maz.org>
2014-07-14 21:00:35 -0700

    month navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @date = Date.today
+    @date = if params[:year].present? && params[:mon].present?
+      Date.parse("#{params[:year]}-#{params[:mon]}-01")
+    else
+      Date.today
+    end
+    @prev_date = @date.beginning_of_month - 1
+    @next_date = @date.end_of_month + 1
     period = [@date.beginning_of_month.beginning_of_week, @date.end_of_month.end_of_week]
     trips = Trip.in_period(*period)
     defaults = (period.first..period.last).each_with_object({}) { |date, m| m[date] = [] }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get ':year/:mon' => 'home#index', as: :home_dated, format: false, constraints: {year: /\d{4}/, mon: /\d{1,2}/}
+
   get 'feed' => 'feed#show', as: :feed
   get 'calendar' => 'calendar#show', as: :calendar
 end
